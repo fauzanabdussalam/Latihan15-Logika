a89cfb7fc9252b75973e574399954945f0f7c6f1
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:10:45 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan15/logika/PBO10K10119901Latihan15Logika.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan15.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Logika
+ *
+ */
+public class PBO10K10119901Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
